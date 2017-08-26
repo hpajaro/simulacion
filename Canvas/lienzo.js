dc8845b670b1aac16609ca78e85fc37abca45590
hpajaro <hpajaro@yahoo.com>
2017-08-25 21:07:56 -0500

    Arbol fractal

---
+++ Canvas/lienzo.js
@@ -0,0 +1,27 @@
+var angulo=0;
+function setup(){
+    createCanvas(500,500);    slider=createSlider(0,PI,PI/4,0.01);
+}
+function draw(){
+    background("#fff");
+    stroke(0);
+  translate(250,height); 
+    rama(150)
+}
+
+function rama(len){
+    
+  line(0,0,0,-len);
+  translate(0,-len);    
+  angulo=slider.value();
+  if(len>4) {
+       push();
+      rotate(angulo);      
+    rama(0.7*len);
+    pop();
+    //push();  
+    rotate(-angulo);
+    rama(0.7*len);
+    //pop();
+  }
+}
